e7fc046d5ab136338b32b93c79a6455e6ddaabb9
edobbin <ethandobbin37@gmail.com>
2023-11-25 13:41:01 -0500

    code question start

--- tech-q/src/firebase.js
+++ tech-q/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyDN0LrBBFaSe0W9i-331CwTI_TM_2EiwYw",
+  authDomain: "techq-5412b.firebaseapp.com",
+  projectId: "techq-5412b",
+  storageBucket: "techq-5412b.appspot.com",
+  messagingSenderId: "75170556973",
+  appId: "1:75170556973:web:a56f7d21dd771e1a83dca4",
+  measurementId: "G-NN5G7L3RX5"
 };
  
 // Initialize Firebase
