40403fdc9b3eb0ae4dff899b1c1fc3f3cb31cf7b
edobbin <ethandobbin37@gmail.com>
2023-11-26 22:32:41 -0500

    questions WIP

--- tech-q/src/components/Question.js
+++ tech-q/src/components/Question.js
@@ -1,28 +1,54 @@
-import React from 'react';
-import './Question.css'
+import React from "react";
+import { NavLink, useNavigate } from 'react-router-dom';
+import { getFirestore } from "firebase/firestore";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
+import "./Question.css";
 
 const Question = () => {
+  const navigate = useNavigate();
   const sectionStyle = {
-    padding: '2%',
-    paddingTop: '5%',
-    alignItema: 'center',
-    maxWidth: '75%',
-    display: 'flex',
-    flexDirection: 'column',
-    textAlign: 'left',
-    margin: '0 auto',
+    padding: "2%",
+    paddingTop: "5%",
+    alignItema: "center",
+    maxWidth: "75%",
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "left",
+    margin: "0 auto",
   };
 
   const footerStyle = {
-    position: 'fixed',
-    bottom: '0',
-    width: '100%',
-    backgroundColor: '#6200B3',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '10px 0',
-    marginTop: 'auto',
-  }
+    position: "fixed",
+    bottom: "0",
+    width: "100%",
+    backgroundColor: "#6200B3",
+    color: "#fff",
+    textAlign: "center",
+    padding: "10px 0",
+    marginTop: "auto",
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+try {
+  const docRef = await addDoc(collection(db, "Post"), {
+    username: '',
+    points: 0,
+    upvotes: 0,
+    downvotes: 0,
+    
+  });
+  console.log("Document written with ID: ", docRef.id);
+  alert("Question posted");
+      navigate("/");
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+      
+  };
 
   return (
     <>
@@ -31,19 +57,31 @@ const Question = () => {
         <form action="post_question.php" method="POST">
           <label htmlFor="question">Your Question:</label>
           <textarea id="question" name="question" rows="5" required></textarea>
-          <input type="submit" value="Post Question" />
+          <button type="submit" onClick={onSubmit}>Post Question</button>
         </form>
 
         <h2>Your Previous Questions</h2>
         <ul>
-          <li><a className="prevQuestions" href="/question1">How to use JavaScript to create interactive web pages?</a></li>
-          <li><a className="prevQuestions" href="/question2">What are the best practices for responsive web design?</a></li>
+          <li>
+            <a className="prevQuestions" href="/question1">
+              How to use JavaScript to create interactive web pages?
+            </a>
+          </li>
+          <li>
+            <a className="prevQuestions" href="/question2">
+              What are the best practices for responsive web design?
+            </a>
+          </li>
           {/* Add more previous questions here */}
         </ul>
       </section>
 
       <footer style={footerStyle}>
-        <p>&copy; 2023 TechQ. All rights reserved. <a href="/privacy">Privacy Policy</a> | <a href="/terms">Terms of Service</a></p>
+        <p>
+          &copy; 2023 TechQ. All rights reserved.{" "}
+          <a href="/privacy">Privacy Policy</a> |{" "}
+          <a href="/terms">Terms of Service</a>
+        </p>
       </footer>
     </>
   );
