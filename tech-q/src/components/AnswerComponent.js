8f94e01634f9d07702a6c4f1fd5cf45c84e48a59
Katie <katherinerlim@gmail.com>
2023-12-04 21:09:38 -0500

    all questions are displaying

--- tech-q/src/components/AnswerComponent.js
+++ tech-q/src/components/AnswerComponent.js
@@ -1,39 +1,69 @@
-import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
-import { db } from '../firebase'; // Adjust the import path as per your project structure
+// AnswerComponent.js
+import React from 'react';
 
-class Answer {
-  constructor(answer, creator_user_ID, question_ID) {
-    this.answer = answer;
-    this.creator_user_ID = creator_user_ID;
-    this.question_ID = question_ID;
-    this.answer_votes = 0; // Initialize answer votes to 0
-    this.answers_date_time = new Date(); // Current timestamp
-    this.answer_ID = null; // Will be set after saving the document
-  }
-
-  async save() {
-    try {
-      const docRef = await addDoc(collection(db, "answers"), this);
-      this.answer_ID = docRef.id;
-      console.log("Answer saved with ID: ", docRef.id);
-    } catch (error) {
-      console.error("Error adding document: ", error);
-    }
-  }
-
-  async delete() {
-    if (!this.answer_ID) {
-      console.error("Error: No ID found for this answer.");
-      return;
-    }
-
-    try {
-      await deleteDoc(doc(db, "answers", this.answer_ID));
-      console.log("Answer deleted with ID: ", this.answer_ID);
-    } catch (error) {
-      console.error("Error deleting document: ", error);
-    }
-  }
+function formatDate(date) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString(undefined, options);
 }
 
-export default Answer;
+const AnswerComponent = ({ answers }) => {
+  if (!answers || answers.length === 0) {
+    return <p>No answers yet.</p>;
+  }
+
+  return (
+    <div>
+      <h4>Answers:</h4>
+      {answers.map((answer, index) => (
+        <div key={index} style={{ border: '1px solid #ddd', padding: '5px', margin: '5px 0' }}>
+          <p>{answer.content}</p>
+          <p>Answered by: {answer.created_by_user}</p>
+          <p>Date posted: {formatDate(answer.date_created?.toDate())}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnswerComponent;
+
+
+// import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
+// import { db } from '../firebase'; // Adjust the import path as per your project structure
+
+// class Answer {
+//   constructor(answer, creator_user_ID, question_ID) {
+//     this.answer = answer;
+//     this.creator_user_ID = creator_user_ID;
+//     this.question_ID = question_ID;
+//     this.answer_votes = 0; // Initialize answer votes to 0
+//     this.answers_date_time = new Date(); // Current timestamp
+//     this.answer_ID = null; // Will be set after saving the document
+//   }
+
+//   async save() {
+//     try {
+//       const docRef = await addDoc(collection(db, "answers"), this);
+//       this.answer_ID = docRef.id;
+//       console.log("Answer saved with ID: ", docRef.id);
+//     } catch (error) {
+//       console.error("Error adding document: ", error);
+//     }
+//   }
+
+//   async delete() {
+//     if (!this.answer_ID) {
+//       console.error("Error: No ID found for this answer.");
+//       return;
+//     }
+
+//     try {
+//       await deleteDoc(doc(db, "answers", this.answer_ID));
+//       console.log("Answer deleted with ID: ", this.answer_ID);
+//     } catch (error) {
+//       console.error("Error deleting document: ", error);
+//     }
+//   }
+// }
+
+// export default Answer;
--- tech-q/src/components/Home.js
+++ tech-q/src/components/Home.js
@@ -1,22 +1,31 @@
-// Home.js
 import React, { useEffect, useState } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc, doc, updateDoc, arrayUnion } from 'firebase/firestore';
 import { db } from '../firebase';
-
+import AnswerComponent from './AnswerComponent';
 
 function formatDate(date) {
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
-
 const Home = () => {
   const [questions, setQuestions] = useState([]);
+  const [answerTextareaValue, setAnswerTextareaValue] = useState('');
   const navigate = useNavigate();
+  
+  const fetchQuestions = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'Post'));
+      const questionData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setQuestions(questionData);
+    } catch (error) {
+      console.error('Error fetching questions:', error);
+    }
+  };
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -31,15 +40,7 @@ const Home = () => {
       }
     });
 
-    const fetchQuestions = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, 'Post'));
-        const questionData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-        setQuestions(questionData);
-      } catch (error) {
-        console.error('Error fetching questions:', error);
-      }
-    };
+    
 
     fetchQuestions();
   }, []);
@@ -56,32 +57,29 @@ const Home = () => {
       });
   };
 
-  const sectionStyle = {
-    padding: '2%',
-    paddingTop: '5%',
-    alignItema: 'center',
-    maxWidth: '75%',
-    display: 'flex',
-    flexDirection: 'column',
-    textAlign: 'left',
-    margin: '0 auto',
-  };
-
-  const footerStyle = {
-    position: 'fixed',
-    bottom: '0',
-    width: '100%',
-    backgroundColor: '#6200B3',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '10px 0',
-    marginTop: 'auto',
+  const handleAnswerSubmit = async (questionId) => {
+    try {
+      const questionRef = doc(db, 'Post', questionId);
+      await updateDoc(questionRef, {
+        answers: arrayUnion({
+          content: answerTextareaValue,
+          created_by_user: auth.currentUser.uid,
+          date_created: new Date(),
+        }),
+      });
+      setAnswerTextareaValue('');
+      console.log('Answer posted successfully');
+      // You may want to fetch questions again to get the updated data
+      fetchQuestions();
+    } catch (error) {
+      console.error('Error adding answer:', error);
+    }
   };
 
   return (
     <>
-      <section style={sectionStyle}>
-        <h1 style={{ textAlign: 'center' }}>Home Page</h1>
+      <section>
+        <h1>Home Page</h1>
         <button onClick={handleLogout}>Logout</button>
 
         {/* Display Questions */}
@@ -99,14 +97,21 @@ const Home = () => {
               >
                 <h3>{question.content}</h3>
                 <p>Created by: {question.created_by_user}</p>
-                <p>Date posted: {formatDate(question.date_created.toDate())}</p>
+                <p>Date posted: {formatDate(question.date_created?.toDate())}</p>
+                <AnswerComponent answers={question.answers} />
+                <textarea
+                  value={answerTextareaValue}
+                  onChange={(e) => setAnswerTextareaValue(e.target.value)}
+                  placeholder="Your Answer"
+                />
+                <button onClick={() => handleAnswerSubmit(question.id)}>Post Answer</button>
                 {/* Display other details of the question */}
               </div>
             ))}
           </div>
         </div>
       </section>
-      <footer style={footerStyle}>
+      <footer>
         <p>
           &copy; 2023 TechQ. All rights reserved. <a href="/privacy">Privacy Policy</a> |{' '}
           <a href="/terms">Terms of Service</a>
@@ -117,3 +122,125 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+// // Home.js
+// import React, { useEffect, useState } from 'react';
+// import { onAuthStateChanged } from 'firebase/auth';
+// import { auth } from '../firebase';
+// import { signOut } from 'firebase/auth';
+// import { useNavigate } from 'react-router-dom';
+// import { collection, getDocs } from 'firebase/firestore';
+// import { db } from '../firebase';
+
+
+// function formatDate(date) {
+//   const options = { year: 'numeric', month: 'long', day: 'numeric' };
+//   return new Date(date).toLocaleDateString(undefined, options);
+// }
+
+
+// const Home = () => {
+//   const [questions, setQuestions] = useState([]);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         // User is signed in, see docs for a list of available properties
+//         // https://firebase.google.com/docs/reference/js/firebase.User
+//         const uid = user.uid;
+//         console.log('uid', uid);
+//       } else {
+//         // User is signed out
+//         console.log('user is logged out');
+//       }
+//     });
+
+//     const fetchQuestions = async () => {
+//       try {
+//         const querySnapshot = await getDocs(collection(db, 'Post'));
+//         const questionData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+//         setQuestions(questionData);
+//       } catch (error) {
+//         console.error('Error fetching questions:', error);
+//       }
+//     };
+
+//     fetchQuestions();
+//   }, []);
+
+//   const handleLogout = () => {
+//     signOut(auth)
+//       .then(() => {
+//         // Sign-out successful.
+//         navigate('/');
+//         console.log('Signed out successfully');
+//       })
+//       .catch((error) => {
+//         // An error happened.
+//       });
+//   };
+
+//   const sectionStyle = {
+//     padding: '2%',
+//     paddingTop: '5%',
+//     alignItema: 'center',
+//     maxWidth: '75%',
+//     display: 'flex',
+//     flexDirection: 'column',
+//     textAlign: 'left',
+//     margin: '0 auto',
+//   };
+
+//   const footerStyle = {
+//     position: 'fixed',
+//     bottom: '0',
+//     width: '100%',
+//     backgroundColor: '#6200B3',
+//     color: '#fff',
+//     textAlign: 'center',
+//     padding: '10px 0',
+//     marginTop: 'auto',
+//   };
+
+//   return (
+//     <>
+//       <section style={sectionStyle}>
+//         <h1 style={{ textAlign: 'center' }}>Home Page</h1>
+//         <button onClick={handleLogout}>Logout</button>
+
+//         {/* Display Questions */}
+//         <div>
+//           <h2>All Questions</h2>
+//           <div style={{ display: 'flex', flexDirection: 'column' }}>
+//             {questions.map((question) => (
+//               <div
+//                 key={question.id}
+//                 style={{
+//                   marginBottom: '20px',
+//                   border: '1px solid #ddd',
+//                   padding: '10px',
+//                 }}
+//               >
+//                 <h3>{question.content}</h3>
+//                 <p>Created by: {question.created_by_user}</p>
+//                 <p>Date posted: {formatDate(question.date_created.toDate())}</p>
+//                 {/* Display other details of the question */}
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//       <footer style={footerStyle}>
+//         <p>
+//           &copy; 2023 TechQ. All rights reserved. <a href="/privacy">Privacy Policy</a> |{' '}
+//           <a href="/terms">Terms of Service</a>
+//         </p>
+//       </footer>
+//     </>
+//   );
+// };
+
+// export default Home;
